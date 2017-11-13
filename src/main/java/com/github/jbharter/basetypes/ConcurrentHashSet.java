403ab0375b840b7ac51503d5284811b82145f997
jharte <jacob.harter@acxiom.com>
2017-11-13 09:37:43 -0600

    Moved basetypes, realized my ignorance on ConcurrentHashSet,

--- src/main/java/com/github/jbharter/basetypes/ConcurrentHashSet.java
+++ src/main/java/com/github/jbharter/basetypes/ConcurrentHashSet.java
@@ -11,35 +11,35 @@ import java.util.stream.Stream;
 
 public class ConcurrentHashSet<T> implements Set<T>, Collection<T> {
 
-    private ConcurrentHashMap<T,Boolean> internal;
+    private Set<T> internal;
 
     public ConcurrentHashSet() {
-        this.internal = new ConcurrentHashMap<>();
+        this.internal = ConcurrentHashMap.newKeySet();
     }
 
     public ConcurrentHashSet(ConcurrentHashSet<T> set) {
-        this.internal = new ConcurrentHashMap<>();
-        set.forEach( thing -> this.internal.put(thing,true));
+        this();
+        this.internal.addAll(set);
     }
-    public boolean add(T t)                                 { return this.internal.put(t,true); }
+    public boolean add(T t)                                 { return this.internal.add(t); }
     public boolean remove(Object o)                         { return this.internal.remove(o); }
-    public boolean containsAll(Collection<?> c)             { return this.internal.keySet().containsAll(c); }
-    public boolean addAll(Collection<? extends T> c)        { return this.internal.keySet().addAll(c); }
+    public boolean containsAll(Collection<?> c)             { return this.internal.containsAll(c); }
+    public boolean addAll(Collection<? extends T> c)        { return this.internal.addAll(c); }
     public boolean equals(Object o)                         { return this.internal.equals(o); }
-    public boolean retainAll(Collection<?> c)               { return this.internal.keySet().retainAll(c); }
-    public boolean removeAll(Collection<?> c)               { return this.internal.keySet().removeAll(c); }
-    public boolean removeIf(Predicate<? super T> filter)    { return this.internal.keySet().removeIf(filter); }
-    public boolean isEmpty()                                { return this.internal.keySet().size() == 0; }
-    public boolean contains(Object o)                       { return this.internal.containsKey(o); }
+    public boolean retainAll(Collection<?> c)               { return this.internal.retainAll(c); }
+    public boolean removeAll(Collection<?> c)               { return this.internal.removeAll(c); }
+    public boolean removeIf(Predicate<? super T> filter)    { return this.internal.removeIf(filter); }
+    public boolean isEmpty()                                { return this.internal.size() == 0; }
+    public boolean contains(Object o)                       { return this.internal.contains(o); }
     public void clear()                                     { this.internal.clear(); }
     public void forEach(Consumer<? super T> action)         { new ConcurrentHashSet<>(this).parallelStream().forEach(action); }
-    public Object[] toArray()                               { return this.internal.keySet().toArray(); }
-    public Iterator<T> iterator()                           { return this.internal.keySet().iterator(); }
-    public Spliterator<T> spliterator()                     { return this.internal.keySet().spliterator(); }
-    public Stream<T> stream()                               { return this.internal.keySet().stream(); }
-    public Stream<T> parallelStream()                       { return this.internal.keySet().parallelStream(); }
-    public <T1> T1[] toArray(T1[] a)                        { return this.internal.keySet().toArray(a); }
-    public int size()                                       { return this.internal.keySet().size(); }
+    public Object[] toArray()                               { return this.internal.toArray(); }
+    public Iterator<T> iterator()                           { return this.internal.iterator(); }
+    public Spliterator<T> spliterator()                     { return this.internal.spliterator(); }
+    public Stream<T> stream()                               { return this.internal.stream(); }
+    public Stream<T> parallelStream()                       { return this.internal.parallelStream(); }
+    public <T1> T1[] toArray(T1[] a)                        { return this.internal.toArray(a); }
+    public int size()                                       { return this.internal.size(); }
     public int hashCode()                                   { return this.internal.hashCode(); }
 
 }
